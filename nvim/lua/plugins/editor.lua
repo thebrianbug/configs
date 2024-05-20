2a1dca830d29979ad15d01d8be68af6b73ac6f12
Brian McIlwain <technicallyemployed@gmail.com>
2024-05-20 09:50:45 -0300

    Working filebrowser replacemet

---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,13 @@
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+
+    "echasnovski/mini.files",
+    lazy = false,
+    opts = {
+      options = {
+        use_as_default_explorer = true,
+      },
+    },
+  },
+}
