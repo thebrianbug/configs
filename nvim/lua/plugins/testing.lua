e1a439ef20d8113ef6330e98dad078a71baaa721
Brian McIlwain <technicallyemployed@gmail.com>
2024-05-23 16:19:44 -0300

    Remove playwright test adapter

--- nvim/lua/plugins/testing.lua
+++ nvim/lua/plugins/testing.lua
@@ -3,22 +3,13 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "marilari88/neotest-vitest",
-      "thenbe/neotest-playwright",
     },
-    opts = function()
-      local neotest_playwright = require("neotest-playwright")
-
-      return {
-        adapters = {
-          ["neotest-vitest"] = {
-            vitestCommand = "npx vitest",
-          },
-          neotest_playwright.adapter({
-            persist_project_selection = true,
-            enable_dynamic_test_discovery = true,
-          }),
+    opts = {
+      adapters = {
+        ["neotest-vitest"] = {
+          vitestCommand = "npx vitest",
         },
-      }
-    end,
+      },
+    },
   },
 }
