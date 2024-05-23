42cd0b9c76893cb1c576d638573ccf48c7f23a02
Brian McIlwain <technicallyemployed@gmail.com>
2024-05-23 16:10:56 -0300

    Playwright test adapter start

--- nvim/lua/plugins/testing.lua
+++ nvim/lua/plugins/testing.lua
@@ -3,13 +3,22 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "marilari88/neotest-vitest",
+      "thenbe/neotest-playwright",
     },
-    opts = {
-      adapters = {
-        ["neotest-vitest"] = {
-          vitestCommand = "npx vitest",
+    opts = function()
+      local neotest_playwright = require("neotest-playwright")
+
+      return {
+        adapters = {
+          ["neotest-vitest"] = {
+            vitestCommand = "npx vitest",
+          },
+          neotest_playwright.adapter({
+            persist_project_selection = true,
+            enable_dynamic_test_discovery = true,
+          }),
         },
-      },
-    },
+      }
+    end,
   },
 }
